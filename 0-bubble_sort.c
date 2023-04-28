d77a2c3f2fcddd65cb80e1ad7d287d61cb64fc28
Sylvain T <sylvaintagnabou@gmail.com>
2023-04-28 22:45:02 +0000

    (done) bubble_sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -34,3 +34,18 @@ if (sorted == 0)
 break;
 }
 }
+
+void print_array(const int *array, size_t size)
+{
+size_t i;
+
+i = 0;
+while (array && i < size)
+{
+if (i > 0)
+printf(", ");
+printf("%d", array[i]);
+++i;
+}
+printf("\n");
+}
